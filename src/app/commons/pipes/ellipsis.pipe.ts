915ee6385766b8f0ee77b83bca816fde6063a6ec
Haldun Atar <atarhaldun@gmail.com>
2017-06-23 02:27:41 +0200

    remove unncessary lines

--- src/app/commons/pipes/ellipsis.pipe.ts
+++ src/app/commons/pipes/ellipsis.pipe.ts
@@ -16,20 +16,3 @@ export class Ellipsis implements PipeTransform {
 
     }
 }
-
-// @Pipe({
-//     name: 'ellipsis'
-// })
-// export class EllipsisPipe {
-//     transform(val, args) {
-//         if (args === undefined) {
-//             return val;
-//         }
-//
-//         if (val.length > args) {
-//             return val.substring(0, args) + '...';
-//         } else {
-//             return val;
-//         }
-//     }
-// }
