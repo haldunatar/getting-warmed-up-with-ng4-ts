88023f679be3191c2e33e5e7ee9017a4200377f1
Haldun Atar <atarhaldun@gmail.com>
2018-01-27 19:53:57 +0100

    fix reducer tests

--- src/app/todo/store/reducers/todo.reducer.spec.ts
+++ src/app/todo/store/reducers/todo.reducer.spec.ts
@@ -2,30 +2,78 @@
 import * as todoStore from '../';
 import { Todo } from '../../model/todo';
 
+let initialState;
+
 describe('Todo Reducer', () => {
+
+	beforeEach(() => {
+		initialState = {
+			activeTodos		: [],
+			completedTodos	: [],
+			loading			: false,
+			todos			: []
+		};
+	});
 
 	describe('Load', () => {
 		it('should start loading todos', () => {
-			const initialState = { todos: [] };
 			const action = new todoStore.TodoLoad();
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual({loading: true, todos: initialState.todos});
+			expect(state).toEqual({
+				activeTodos		: [],
+				completedTodos	: [],
+				loading			: true,
+				todos			: []
+		});
 		});
 
 		it('should load todos', () => {
-			const initialState = { todos: [] };
 			const todoList = [{_id: '123sed', title: 'windsurfing', status: false}];
 			const action = new todoStore.TodoLoadSucceeded(todoList);
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual({loading: false, todos: todoList});
+			expect(state).toEqual({
+				activeTodos		: [{_id: '123sed', title: 'windsurfing', status: false}],
+				completedTodos	: [],
+				loading			: false,
+				todos			: [{_id: '123sed', title: 'windsurfing', status: false}]
+		});
+		});
+
+		it('should filter active and completed todos correctly', () => {
+			const todoList = [
+				{_id: 'aaa', title: 'windsurfing', status: false},
+				{_id: 'bbb', title: 'partying', status: false},
+				{_id: 'ccc', title: 'shopping', status: true},
+				{_id: 'ddd', title: 'studying', status: true}
+			];
+			const action = new todoStore.TodoLoadSucceeded(todoList);
+
+			const state = todoStore.todoReducer(initialState, action);
+
+			expect(state).toEqual({
+				activeTodos		: [
+					{_id: 'aaa', title: 'windsurfing', status: false},
+					{_id: 'bbb', title: 'partying', status: false},
+				],
+				completedTodos	: [
+					{_id: 'ccc', title: 'shopping', status: true},
+					{_id: 'ddd', title: 'studying', status: true}
+				],
+				loading			: false,
+				todos			: [
+					{_id: 'aaa', title: 'windsurfing', status: false},
+					{_id: 'bbb', title: 'partying', status: false},
+					{_id: 'ccc', title: 'shopping', status: true},
+					{_id: 'ddd', title: 'studying', status: true}
+				]
+			});
 		});
 
 		it('should give an error message on loading todos', () => {
-			const initialState = { todos: [] };
 			const err = { err: '500', statusText: 'server error'};
 			const action = new todoStore.TodoLoadFailed(err);
 
@@ -33,43 +81,45 @@ describe('Todo Reducer', () => {
 
 			expect(state).toEqual(
 				{
-					loading: false, 
-					todos: initialState.todos, 
-					error: 'Todo cannot be retrieved!'
+					activeTodos		: [],
+					completedTodos	: [],
+					error 			: 'Todos cannot be retrieved!',
+					loading			: false,
+					todos			: []
 				});
 		});
 	});
 
 	describe('Add', () => {
 		it('should start add todo', () => {
-			const initialState = { todos: [] };
 			const newTodo = {title: 'meeting', status: false};
 			const action = new todoStore.TodoAdd(newTodo);
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual({loading: true, todos: initialState.todos});
+			expect(state).toEqual({
+				activeTodos		: [],
+				completedTodos	: [],
+				loading			: true,
+				todos			: []
+		});
 		});
 
 		it('should return state with new todo', () => {
-			const initialState = { todos: [] };
-			const newTodo: Todo = {title: 'meeting', status: false, _id: '3434ssd'};
+			const newTodo: Todo[] = [{title: 'meeting', status: false, _id: 'someId'}];
 			const action = new todoStore.TodoAddSucceeded(newTodo);
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual(
-				{
-					loading: false,  
-					todos: [
-						{ title: 'meeting', status: false, _id: '3434ssd'}
-					]
-				}
-			);
+			expect(state).toEqual({
+					activeTodos		: [{title: 'meeting', status: false, _id: 'someId'}],
+					completedTodos	: [],
+					loading			: false,
+					todos			: [{title: 'meeting', status: false, _id: 'someId'}]
+		});
 		});
 
 		it('should give an error message on adding a todo', () => {
-			const initialState = { todos: [] };
 			const err = { err: '500', statusText: 'server error'};
 			const action = new todoStore.TodoAddFailed(err);
 
@@ -77,9 +127,11 @@ describe('Todo Reducer', () => {
 
 			expect(state).toEqual(
 				{
-					loading: false, 
-					todos: initialState.todos, 
-					error: 'Add new todo action is failed'
+					activeTodos		: [],
+					completedTodos	: [],
+					error 			: 'Add new todo action is failed',
+					loading			: false,
+					todos			: []
 				}
 			);
 		});
@@ -87,26 +139,29 @@ describe('Todo Reducer', () => {
 
 	describe('Update', () => {
 		it('should start updating a todo', () => {
-			const initialState = { todos: [{_id: "123", title: 'todo title', status:false}] }; 
+			initialState = { 
+				todos: [{_id: "123", title: 'todo title', status:false }],
+				activeTodos		: [{_id: "123", title: 'todo title', status:false }],
+				completedTodos	: [],
+				loading			: true
+			}; 
 			const payload = {_id: "123", title: 'new title'};
 			const action = new todoStore.TodoUpdate(payload);
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual(
-				{
-					loading	: true,
-					todos	: initialState.todos
-				}
-			);
+			expect(state).toEqual(initialState);
 		});
 
 		it('should return updated todos state', () => {
-			const initialState = { 
+			initialState = { 
 				todos: [
 					{_id: "123", title: 'todo title', status:false},
 					{_id: "asd43", title: 'other todo title', status: true},
-				] 
+				],
+				activeTodos		: [{_id: "123", title: 'todo title', status:false}],
+				completedTodos	: [{_id: "asd43", title: 'other todo title', status: true}],
+				loading			: false
 			}; 
 			const payload = {_id: "123", title: 'new title'};
 			const action = new todoStore.TodoUpdateSucceeded(payload);
@@ -115,17 +170,18 @@ describe('Todo Reducer', () => {
 
 			expect(state).toEqual(
 				{
-					loading	: false,
-					todos	: [
-						{_id: "123", title: 'new title', status: false},
-						{_id: "asd43", title: 'other todo title', status: true}
-					]
+					todos: [
+						{_id: "123", title: 'new title', status:false},
+						{_id: "asd43", title: 'other todo title', status: true},
+					],
+					activeTodos		: [{_id: "123", title: 'new title', status:false}],
+					completedTodos	: [{_id: "asd43", title: 'other todo title', status: true}],
+					loading			: false
 				}
 			);
 		});
 
 		it('should give an error message on toggling a todo', () => {
-			const initialState = { todos: [] };
 			const err = { err: '500', statusText: 'server error'};
 			const action = new todoStore.TodoUpdateFailed(err);
 
@@ -133,9 +189,11 @@ describe('Todo Reducer', () => {
 
 			expect(state).toEqual(
 				{
-					loading: false, 
-					todos: initialState.todos, 
-					error: 'Todo cannot be updated!'
+					activeTodos		: [],
+					completedTodos	: [],
+					error 			: 'Todo cannot be updated!',
+					loading			: false,
+					todos			: []
 				}
 			);
 		});
@@ -143,22 +201,27 @@ describe('Todo Reducer', () => {
 
 	describe('Toggle', () => {
 		it('should start toggling a todo', () => {
-			const initialState = { todos: [{_id: "123", title: 'todo title', status:false }] }; 
+			initialState = { 
+				todos: [{_id: "123", title: 'todo title', status:false }],
+				activeTodos		: [{_id: "123", title: 'todo title', status:false}],
+				completedTodos	: [],
+				loading			: true
+			}; 
 			const payload = {_id: "123", status: true};
 			const action = new todoStore.TodoToggle(payload);
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual(
-				{
-					loading	: true,
-					todos	: initialState.todos
-				}
-			);
+			expect(state).toEqual(initialState);
 		});
 
 		it('should return toggled state', () => {
-			const initialState = { todos: [{_id: "123", title: 'todo title', status: false }] }; 
+			initialState = { 
+				todos: [{_id: "123", title: 'todo title', status: false }],
+				activeTodos		: [{_id: "123", title: 'todo title', status: false }],
+				completedTodos	: [],
+				loading			: false
+			}; 
 			const payload = {_id: "123", status: true};
 			const action = new todoStore.TodoToggleSucceeded(payload);
 
@@ -166,16 +229,15 @@ describe('Todo Reducer', () => {
 
 			expect(state).toEqual(
 				{
-					loading	: false,
-					todos	: [
-						{_id: "123", title: 'todo title', status: true }
-					]
+					todos: [{_id: "123", title: 'todo title', status: true }],
+					activeTodos		: [],
+					completedTodos	: [{_id: "123", title: 'todo title', status: true }],
+					loading			: false
 				}
 			);
 		});
 
 		it('should give an error message on toggling a todo', () => {
-			const initialState = { todos: [] };
 			const err = { err: '500', statusText: 'server error'};
 			const action = new todoStore.TodoToggleFailed(err);
 
@@ -183,9 +245,11 @@ describe('Todo Reducer', () => {
 
 			expect(state).toEqual(
 				{
-					loading: false, 
-					todos: initialState.todos, 
-					error: 'Todo status cannot be updated!'
+					todos			: [],
+					activeTodos		: [],
+					completedTodos	: [],
+					error			: 'The todo status cannot be updated!',
+					loading			: false
 				}
 			);
 		});
@@ -193,7 +257,12 @@ describe('Todo Reducer', () => {
 
 	describe('Remove', () => {
 		it('should start removing state', () => {
-			const initialState = {loading: true, todos: [{_id: "123", title: 'todo title', status:false }] }; 
+			initialState = {
+				todos: [{_id: "123", title: 'todo title', status:false }],
+				activeTodos		: [{_id: "123", title: 'todo title', status:false }],
+				completedTodos	: [],
+				loading			: true
+			}; 
 			const payload = "123";
 			const action = new todoStore.TodoRemove(payload);
 
@@ -202,31 +271,52 @@ describe('Todo Reducer', () => {
 			expect(state).toEqual(initialState);
 		});
 
-		it('should state with removed todo', () => {
-			const initialState = {loading: true, todos: [
+		it('should update state with removed todo', () => {
+			initialState = {
+				loading: false, 
+				todos: [
 				{_id: "asdas23", title: 'nice todo', status: false },
 				{_id: "wewe23", title: 'boring todo', status: true },
 				{_id: "gdgd34", title: 'scary todo', status: false }
-			]}; 
+				],
+				activeTodos		: [
+					{_id: "asdas23", title: 'nice todo', status: false },
+					{_id: "gdgd34", title: 'scary todo', status: false }
+				],
+				completedTodos	: [	{_id: "wewe23", title: 'boring todo', status: true }]
+			}; 
 
 			const payload = "gdgd34";
 			const action = new todoStore.TodoRemoveSucceeded(payload);
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual({
+			expect(state).toEqual(
+				{
 				loading: false,
 				todos: [
 					{_id: "asdas23", title: 'nice todo', status: false },
 					{_id: "wewe23", title: 'boring todo', status: true }
-				]
-			});
+					],
+					activeTodos		: [
+						{_id: "asdas23", title: 'nice todo', status: false }
+					],
+					completedTodos	: [	{_id: "wewe23", title: 'boring todo', status: true }]
+				}
+			);
 		});
 
 		it('should give an error message on removing a todo', () => {
-			const initialState = {loading: false, todos: [
+			initialState = {
+				loading: false, 
+				todos: [
+					{_id: "asdas23", title: 'nice todo', status: false }
+				],
+				activeTodos		: [
 				{_id: "asdas23", title: 'nice todo', status: false }
-			]}; 
+				],
+				completedTodos	: []
+			}; 
 			const err = { err: '500', statusText: 'server error'};
 			const action = new todoStore.TodoRemoveFailed(err);
 
@@ -235,8 +325,14 @@ describe('Todo Reducer', () => {
 			expect(state).toEqual(
 				{
 					loading: false, 
-					todos: initialState.todos, 
-					error: 'Todo cannot be removed!'
+					todos: [
+						{_id: "asdas23", title: 'nice todo', status: false }
+					],
+					activeTodos		: [
+						{_id: "asdas23", title: 'nice todo', status: false }
+					],
+					completedTodos	: [],
+					error: 'Todo cannnot be removed!'
 				}
 			);
 		});
@@ -244,11 +340,20 @@ describe('Todo Reducer', () => {
 
 	describe('Remove All', () => {
 		it('should start removing all todos', () => {
-			const initialState = {loading: true, todos: [
+			initialState = {
+				loading: true, 
+				todos: [
 				{_id: "asdas23", title: 'nice todo', status: false },
 				{_id: "wewe23", title: 'boring todo', status: true },
 				{_id: "gdgd34", title: 'scary todo', status: false }
-			] }; 
+				],
+				activeTodos		: [
+					{_id: "asdas23", title: 'nice todo', status: false },
+					{_id: "gdgd34", title: 'scary todo', status: false }
+				],
+				completedTodos	: [{_id: "wewe23", title: 'boring todo', status: true }]
+				
+			}; 
 			const action = new todoStore.TodoRemoveAll();
 
 			const state = todoStore.todoReducer(initialState, action);
@@ -256,21 +361,35 @@ describe('Todo Reducer', () => {
 			expect(state).toEqual(initialState);
 		});
 
-		it('should return an empty todos state', () => {
-			const initialState = {loading: true, todos: [
+		it('should clear all todos', () => {
+			initialState = {
+				loading: false, 
+				todos: [
 				{_id: "asdas23", title: 'nice todo', status: false },
 				{_id: "wewe23", title: 'boring todo', status: true },
 				{_id: "gdgd34", title: 'scary todo', status: false }
-			] }; 
+				],
+				activeTodos		: [
+					{_id: "asdas23", title: 'nice todo', status: false },
+					{_id: "gdgd34", title: 'scary todo', status: false }
+				],
+				completedTodos	: [{_id: "wewe23", title: 'boring todo', status: true }]
+			}; 
 			const action = new todoStore.TodoRemoveAllSucceeded();
 
 			const state = todoStore.todoReducer(initialState, action);
 
-			expect(state).toEqual({loading: false, todos: []});
+			expect(state).toEqual(
+				{
+					loading			: false, 
+					todos			: [],
+					activeTodos		: [],
+					completedTodos	: []
+				}
+			);
 		});
 
 		it('should give an error message on removin all todo', () => {
-			const initialState = { todos: [] };
 			const err = { err: '500', statusText: 'server error'};
 			const action = new todoStore.TodoRemoveAllFailed(err);
 
@@ -278,9 +397,11 @@ describe('Todo Reducer', () => {
 
 			expect(state).toEqual(
 				{
-					loading: false, 
-					todos: initialState.todos, 
-					error: 'Todos cannot be removed!'
+					loading			: false, 
+					todos			: [],
+					error			: 'Todos cannot be removed',
+					activeTodos		: [],
+					completedTodos	: []
 				}
 			);
 		});
